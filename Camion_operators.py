5faa42ac46c2f16c69b9f43096d735b63a68f2c6
Kevin Liu Yang <kevin.liu@estudiantat.upc.edu>
2025-11-08 12:11:29 +0100

    Implement various CamionOperators classes

--- Camion_operators.py
+++ Camion_operators.py
@@ -1,2 +1,46 @@
 class CamionOperators(object):
     pass
+
+class MoverPeticion(CamionOperators):
+    def __init__(self, pet_i: int, cam_i: int, cam_j: int):
+        self.pet_i = pet_i
+        self.cam_i = cam_i
+        self.cam_j = cam_j
+
+    def __repr__(self) -> str:
+        return f"Mover la petición {self.pet_i} del camión {self.cam_i} al camión {self.cam_j}"
+
+class AsignarPeticion(CamionOperators):
+    def __init__(self, pet_i: int, cam_i):
+        self.pet_i = pet_i
+        self.cam_i = cam_i
+
+    def __repr__(self) -> str:
+        return f"Asignar petición {self.pet_i} al camión {self.cam_i}"
+
+class SwapPeticiones(CamionOperators):
+    def __init__(self, pet_i: int, pet_j:int, cam_i: int, cam_j: int):
+        self.pet_i = pet_i
+        self.pet_j = pet_j
+        self.cam_i = cam_i
+        self.cam_j = cam_j
+
+    def __repr__(self) -> str:
+        return f"Intercambiar la petición {self.pet_i} del camión {self.cam_i} por la petición {self.pet_j} del camión {self.cam_j}"
+
+class MoverAntes(CamionOperators):
+    def __init__(self, pet_i: int, pos_i: int, pos_j: int):
+        self.pet_i = pet_i
+        self.pos_i = pos_i
+        self.pos_j = pos_j
+
+    def __repr__(self) -> str:
+        return f"Mover la petición {self.pet_i} de la posición {self.pos_i} a la posición {self.pos_j}"
+
+class EliminarPeticiones(CamionOperators):
+    def __init__(self, pet_i: int, cam_i: int):
+        self.pet_i = pet_i
+        self.cam_i = cam_i 
+
+    def __repr__(self) -> str:
+        return f"Eliminar la petición {self.pet_i} del camión {self.cam_i}"
