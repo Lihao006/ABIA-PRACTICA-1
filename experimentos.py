c0b17219a8d468abc011109d377f40b5b269fa83
Lihao006 <lihao.jin@estudiantat.upc.edu>
2025-11-09 16:05:40 +0100

    Update experimentos.py

--- experimentos.py
+++ experimentos.py
@@ -4,3 +4,18 @@ from CamionesyPeticiones_problem import CamionesyPeticiones
 from Camion_parameters import ProblemParameters
 from Camion_operators import *
 from Camion_state import *
+
+if __name__ == '__main__':
+	# parametros por defecto
+	params = ProblemParameters()
+	initial_state = generar_sol_inicial(params)
+	n = hill_climbing(CamionesyPeticiones(initial_state))
+	print(n)
+	print(n.heuristic())
+
+
+
+	# Suposant que la funció mesurar s'astar_search
+	# i volem fer una e x e c u c i :
+	time = timeit.timeit(lambda: hill_climbing(CamionesyPeticiones(initial_state)), number=1)
+	print(time) # Retorna temps en segons
