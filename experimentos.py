2d0c7796962311e5bc96aee0454b570280202d53
Lihao006 <lihao.jin@estudiantat.upc.edu>
2025-11-09 16:00:10 +0100

    Create experimentos.py

---
+++ experimentos.py
@@ -0,0 +1,6 @@
+import timeit
+from aima.search import hill_climbing, simulated_annealing
+from CamionesyPeticiones_problem import CamionesyPeticiones
+from Camion_parameters import ProblemParameters
+from Camion_operators import *
+from Camion_state import *
